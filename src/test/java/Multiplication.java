cb6dc56eab000ac468521bbb534ff46af841b313
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2018-02-26 17:18:10 +0600

    We write test. Code to working without errors.

---
+++ src/test/java/Multiplication.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Multiplication {
+    @Test
+    public void testMultiplication(){
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
+    }
+}
