406d75d38b040d6effa2b32ae97fecffc7cd1bfd
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2018-02-27 05:20:37 +0600

    Implemented a test for equality.

--- src/test/java/Multiplication.java
+++ src/test/java/Multiplication.java
@@ -1,14 +1,22 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class Multiplication {
     @Test
-    public void testMultiplication(){
+    public void testMultiplication() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(30, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
