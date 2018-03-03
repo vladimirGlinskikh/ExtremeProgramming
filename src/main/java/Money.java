90b24d93274ea68a132da735677c9198d903d824
VladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2018-03-03 16:06:08 +0600

    We write testSimpleAddition.

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,4 +1,4 @@
-class Money {
+class Money implements Expression {
     protected int amount;
     protected String currency;
 
@@ -35,5 +35,9 @@ class Money {
                 "amount=" + amount +
                 ", currency='" + currency + '\'' +
                 '}';
+    }
+
+    public Expression plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
     }
 }
