06fa6afc821e8371606262fa4f2b0e47c50c42b1
vladimirGlinskikh <vladimir.v.glinskikh@gmail.com>
2018-03-06 15:34:55 +0600

    Do the real implementation.

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -38,6 +38,11 @@ class Money implements Expression {
     }
 
     public Expression plus(Money addend) {
-        return new Money(amount + addend.amount, currency);
+        return new Sum(this, addend);
+    }
+
+    @Override
+    public Money reduce(String to) {
+        return this;
     }
 }
